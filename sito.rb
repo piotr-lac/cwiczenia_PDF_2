dbab19801566a930ee2c1ff5f1fe735c942933e4
piotr.lac <piotr.lac@protonmail.ch>
2017-10-26 00:26:22 +0200

    poprawki 1

--- sito.rb
+++ sito.rb
@@ -1,13 +1,19 @@
 first, second = ARGV
+first = first.to_i
+second = second.to_i
 
-a = (first.to_i..second.to_i).to_a
+
+first, second = second, first if first > second
+
+a = (first..second).to_a.delete_if{|x| x<=1}
 liczby_pierwsze=[]
-a.delete_if{|x| x<=1}
+
 loop do 
 	liczby_pierwsze << a[0]
 	a.select!{|x| x%a[0]!=0}
 	break if a.length == 0 
 end
 
-print liczby_pierwsze.compact!.empty? ? "brak liczb pierwszych w zasięgu #{first} : #{second}" : liczby_pierwsze
+print liczby_pierwsze.compact.empty? ? "Prime numbers : no prime numbers" : "Prime numbers : #{liczby_pierwsze.join(" ")}"
+
 
